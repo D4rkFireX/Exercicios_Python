099b459569baa4fe379988a11b944a9ce16965b9
Victor Gabriel <vvictorgabrielreis@gmail.com>
2026-05-21 13:09:43 -0300

    refactor: refatoração de código no cadastro_tarefas.py

--- cadastro_tarefas.py
+++ cadastro_tarefas.py
@@ -11,7 +11,11 @@ while True:
     print("2 - Ver a lista de Tarefa")
     print("3 - Remover Tarefa")
     print("4 - Sair")
-    decisao = int(input("Digite o número da operação que deseja: "))
+    try:
+        decisao = int(input("Digite o número da operação que deseja: "))
+    except:
+        print()
+        continue
 
     if decisao == 1:
         while True:
@@ -23,17 +27,22 @@ while True:
                 break
             elif pergunta_add_tarefa in ["sim", "s"]:
                 sair = False
-        if sair:
-            continue
 
     elif decisao == 2:
         if lista_tarefas == []:
             print(f"\nA lista de tarefas está vazia...\n")
         else:
             for tarefa in lista_tarefas:
-                print()
                 print(tarefa)
-                print()
+                
+    elif decisao == 3:
+        if lista_tarefas == []:
+            print(f"\n A lista de tarefas está vazia...\n")
+        else:
+            for indice, tarefa in enumerate(lista_tarefas):
+                print(indice + 1, "-", tarefa)
+            remover = int(input("Qual tarefa quer remover? "))
+            lista_tarefas.pop(remover - 1)
 
     elif decisao == 4:
         break
