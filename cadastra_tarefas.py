a3232b0bd03bdc33c30a65af61995a8f40634164
Victor <vvictorgabrielreis@gmail.com>
2026-05-21 16:28:31 -0300

    refactor: refatora cadastra_tarefas.py

--- cadastra_tarefas.py
+++ cadastra_tarefas.py
@@ -16,8 +16,8 @@ while True:
 
     try:
         decisao = int(input("Digite o NÚMERO da operação desejada: "))
-    except:
-        print()
+    except ValueError:
+        print("Inválido")
         continue
 
     if decisao == 1:
@@ -25,6 +25,10 @@ while True:
             tarefa = input("Digite a tarefa ou 'sair'").strip()
             if tarefa.lower() in ["sair", "sai", "sa"]:
                 break
+            if not tarefa:
+                print("Digite uma tarefa válida!")
+                continue
+            
             lista_tarefas.append(tarefa)
             print("Tarefa adicionada com sucesso!")
             pergunta_add_tarefa = input("Deseja adicionar mais uma tarefa? ").lower()
@@ -49,20 +53,28 @@ while True:
                 print("=" * 4, "TAREFAS", "=" * 4)
                 mostrar_tarefas()
                 print("=" * 16)
-                remover = input("Digite a tarefa que quer remover ou sair: ")
+                remover = input("Digite o NÚMERO da tarefa que quer remover ou sair: ")
                 if remover.lower() in ["sair", "sai", "sa"]:
                     break
                 else:
                     try:
                         remover = int(remover)
-                        lista_tarefas.pop(remover - 1)
-                        print("Tarefa removida!")
-                        pergunta_rem_tarefa = input("Deseja remover mais uma tarefa? ").lower()
-                        if pergunta_rem_tarefa in ["não", "nao", "na", "nã", "n"]:
-                            break
-                        elif pergunta_rem_tarefa in ["sim", "si", "s"]:
-                            continue
-                    except:
-                        print()
+                        
+                        if 1 <= remover <= len(lista_tarefas):
+                            
+                            lista_tarefas.pop(remover - 1)
+                            print("Tarefa removida!")
+                            pergunta_rem_tarefa = input("Deseja remover mais uma tarefa? ").lower()
+                            
+                            if pergunta_rem_tarefa in ["não", "nao", "na", "nã", "n"]:
+                                break
+                            
+                            elif pergunta_rem_tarefa in ["sim", "si", "s"]:
+                                continue
+                        else:
+                            print("Tarefa Inválida")
+                            
+                    except ValueError:
+                        print("Inválido")
     elif decisao == 4:
         break
